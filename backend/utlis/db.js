40c613de82f47c55a8c3d12f9f249c0ea89df82a
Hafsa riaz <121846771+hafsoo@users.noreply.github.com>
2025-01-20 11:59:31 +0500

    Update db.js

--- backend/utlis/db.js
+++ backend/utlis/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 
 const dbCon=async()=>{
     try {
-        await mongoose.connect("mongodb+srv://hafsariaz:hafsa123@cluster0.i0joy.mongodb.net/crudretryWrites=true&w=majority&appName=Cluster0")
+        await mongoose.connect(process.env.URL)
         console.log('Mongodb is conntected')
     } catch (error) {
         console.log(error)
