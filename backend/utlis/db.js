0f8fbfaba596d366ac7f48f16d6258f69b754389
Hafsa riaz <121846771+hafsoo@users.noreply.github.com>
2025-01-20 12:01:39 +0500

    Update db.js

--- backend/utlis/db.js
+++ backend/utlis/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 
 const dbCon=async()=>{
     try {
-        await mongoose.connect(process.env.URL)
+        await mongoose.connect(process.env.DB_URL)
         console.log('Mongodb is conntected')
     } catch (error) {
         console.log(error)
